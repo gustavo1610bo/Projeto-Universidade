17e0db50722da68bce4217b8093aa452569255e2
Gustavo Guimarães <gustavodossantosguimaraes93@gmail.com>
2025-03-11 22:13:37 -0300

    ajustes

---
+++ atividade_2/script.js
@@ -0,0 +1,15 @@
+function SOMAR(num1,num2){
+    alert(num1+num2);
+}
+
+function SUBTRAIR(num1,num2){
+    alert(num1-num2);
+}
+
+function MULTIPLICAR(num1,num2){
+    alert(num1*num2);
+}
+
+function DIVIDIR(num1,num2){
+    alert(num1/num2);
+}
